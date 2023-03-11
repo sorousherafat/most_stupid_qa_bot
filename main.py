f433a6ed6f1e92799c4c2ed29f778eb6846d2f72
Soroush Sherafat <sorousherafat@gmail.com>
2023-03-11 16:15:35 +0330

    added qa functionality

---
+++ main.py
@@ -0,0 +1,28 @@
+from flask import Flask, request
+import pandas as pd
+
+app = Flask(__name__)
+
+df = pd.read_csv("qa.csv")
+
+
+@app.route("/ping")
+def ping():
+    app.logger.debug("ping.")
+    return "pong"
+
+
+@app.route("/qa", methods=["POST"])
+def qa():
+    question = request.json["question"]
+    qa = df.loc[df["question"] == question, "answer"]
+    if qa.empty:
+        app.logger.info(f"question: `{question}' was not found.")
+        return "question not found", 404
+    answer = qa.iloc[0]
+    app.logger.debug(f"answered: `{answer}' to question: `{question}'")
+    return {"answer": answer}
+
+
+if __name__ == "__main__":
+    app.run()
